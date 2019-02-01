fcce72d4dfd16c4e7e886f57090f5c51caaf58e0
Angela Melinda <angela.melinda@shopee.com>
2019-02-01 15:55:11 +0700

    Add conditional test

--- src/csvautofilljs.ts
+++ src/csvautofilljs.ts
@@ -109,7 +109,7 @@ const CsvAutoFill = {
       (f && f.name.substr(f.name.length - 4) === '.csv' && f.type === 'text/csv') ||
       (f && f.name.substr(f.name.length - 4) === '.csv' && f.type === 'application/vnd.ms-excel')
     ) {
-      console.log('upload file 1')
+      console.log('upload file #1')
       parse(f, {
         complete: (result: any) => {
           let resultArr = result.data
@@ -133,7 +133,7 @@ const CsvAutoFill = {
         }
       })
     } else {
-      console.log('upload file 2')
+      console.log('upload file #2')
       return {
         data: {
           message: 'Please choose a csv file'
